89ce05cfde206e92fb1bb56a3381fff015a3874a
vladvaldtitov <uplight.ca@gmail.com>
2016-03-10 12:41:03 -0500

    success emailed

--- sendMessage.php
+++ sendMessage.php
@@ -9,6 +9,6 @@ $_SESSION['time'] = $time;
 
 
 if($timeout<10)echo 'error '.$timeout;
-else echo mail('uplight.ca@gmail.com','Website contact ',file_get_contents('php://input')."\n".date("Y-m-d H:i:s")."\n\r")?'success':'error write';
+else echo mail('uplight.ca@gmail.com','Website contact ',file_get_contents('php://input')."\n".date("Y-m-d H:i:s")."\n\r")?'success emailed':'error write';
 
 ?>
